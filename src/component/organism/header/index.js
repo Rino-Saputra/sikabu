707deaac77b7b662bc35bdd47d9fb7f96b9cb8c8
karunialeo <karunialeo@gmail.com>
2022-05-06 11:26:57 +0700

    navbar and header styling edited --karunialeo--

--- src/component/organism/header/index.js
+++ src/component/organism/header/index.js
@@ -1,29 +1,39 @@
-import { Container, Navbar, Nav } from 'react-bootstrap'
-import { logo } from '../../../export/image'
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { logo } from "../../../export/image";
 
-import './style.css'
+import "./style.css";
 
 export default function Header() {
-    return (
-        <Navbar collapseOnSelect expand="md">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <div className='d-flex align-items-center'>
-                        <img src={logo} alt="" className='header-logo me-2' />
-                        <h2 style={{color: '#1e1913'}} className='title-brand mt-2'>CKBU</h2>
-                    </div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto d-flex align-items-center fw-bold text-white">
-                        <Nav.Link className='a1' href="#features">Features <div className='b1'></div> </Nav.Link>
-                        <Nav.Link className='a2' href="#pricing">Tour <div className='b2'></div></Nav.Link>
-                        <Nav.Link className='a3' href="#deets">Pricing <div className='b3'></div></Nav.Link>
-                        <Nav.Link className='a4' href="#deet">Contact <div className='b4'></div></Nav.Link>
-                        <button className='login mx-2 my-2 a5'>Login</button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar collapseOnSelect expand="md">
+      <Container>
+        <Navbar.Brand href="#home">
+          <div className="d-flex align-items-center">
+            <img src={logo} alt="" className="header-logo me-3" />
+            <h2 style={{ color: "#1e1913" }} className="title-brand mt-2">
+              CKBU
+            </h2>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto d-flex align-items-center fw-bold text-white">
+            <Nav.Link className="a1" href="#features">
+              Features <div className="b1"></div>{" "}
+            </Nav.Link>
+            <Nav.Link className="a2" href="#pricing">
+              Tour <div className="b2"></div>
+            </Nav.Link>
+            <Nav.Link className="a3" href="#deets">
+              Pricing <div className="b3"></div>
+            </Nav.Link>
+            <Nav.Link className="a4" href="#deet">
+              Contact <div className="b4"></div>
+            </Nav.Link>
+            <button className="login mx-2 my-2 a5">Login</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- src/component/template/herosection/Index.js
+++ src/component/template/herosection/Index.js
@@ -1,23 +1,23 @@
-import './style.css'
-import { Container, Row, Col } from 'react-bootstrap'
-import Header from "../../organism/header"
-import TextSection from '../../organism/textSection'
-import { hero1 } from '../../../export/image'
+import "./style.css";
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "../../organism/header";
+import TextSection from "../../organism/textSection";
+import { hero1 } from "../../../export/image";
 
 export default function HeroSection() {
   return (
-    <Container fluid className='mb-5'>
+    <Container fluid className="mb-5">
       <Header />
-      <Container className='pt-4'>
-        <Row className='d-flex align-items-center justify-content-between'>
-          <Col md={5} className='mb-5'>
+      <Container className="pt-4">
+        <Row className="d-flex align-items-center justify-content-between">
+          <Col md={5} className="mb-5">
             <TextSection />
           </Col>
-          <Col md={7} className='text-end'>
-            <img src={hero1} className='img-fluid' />
+          <Col md={7} className="text-end">
+            <img src={hero1} className="img-fluid" />
           </Col>
         </Row>
       </Container>
     </Container>
-  )
+  );
 }
